e2e2e6229272d36a9b02de18ed43c517ce5c4a58
kianakarimi04 <kianakarimi8383@gmail.com>
2026-02-19 09:19:12 -0800

    Implement create socket section

---
+++ server.c
@@ -0,0 +1,28 @@
+//
+// Created by kiana on 2/19/26.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE];
+
+    //create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket failed");
+        exit(1);
+    }
+
+    return 0;
+}
